ed7847d9df37d9b2cc3c9524b9e0b71255dabe0c
Vladislav Demyanov <v.demyanov.dev@gmail.com>
2024-02-21 17:11:02 +0300

    feat: add patients controller

---
+++ HCSYS.Api/Controllers/PatientsController.cs
@@ -0,0 +1,84 @@
+﻿using HCSYS.Api.Models;
+using HCSYS.Core.Models;
+using HCSYS.Core.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HCSYS.Api.Controllers;
+
+/// <summary>
+/// Manages patients.
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class PatientsController : ControllerBase
+{
+    private readonly IPatientsService _patientsService;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PatientsController"/> class.
+    /// </summary>
+    /// <param name="patientsService">The <see cref="IPatientsService"/> instance.</param>
+    public PatientsController(IPatientsService patientsService)
+    {
+        _patientsService = patientsService ?? throw new ArgumentNullException(nameof(patientsService));
+    }
+
+    /// <summary>
+    /// Creates a new patient.
+    /// </summary>
+    /// <param name="request">Data to create a new patient.</param>
+    /// <returns>Data transfer object of created patient entity or expected errors.</returns>
+    [HttpPost]
+    [ProducesResponseType(typeof(PatientDto), StatusCodes.Status201Created)]
+    public async Task<ActionResult<PatientDto>> Create([FromBody] CreatePatientRequest request)
+    {
+        PatientDto createdPatient = await _patientsService.CreateAsync(request);
+
+        return StatusCode(StatusCodes.Status201Created, createdPatient);
+    }
+
+    /// <summary>
+    /// Gets an existing patient.
+    /// </summary>
+    /// <param name="patientId">Patient's identifier.</param>
+    /// <returns>Data transfer object of existing patient entity or expected errors.</returns>
+    [HttpGet("{patientId}")]
+    [ProducesResponseType(typeof(PatientDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<PatientDto>> GetById(Guid patientId)
+    {
+        PatientDto patient = await _patientsService.GetByIdAsync(patientId);
+
+        return StatusCode(StatusCodes.Status200OK, patient);
+    }
+
+    /// <summary>
+    /// Deletes an existing patient.
+    /// </summary>
+    /// <param name="patientId">Patient's identifier.</param>
+    /// <returns>Empty result or expected errors.</returns>
+    [HttpDelete("{patientId}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> Delete(Guid patientId)
+    {
+        await _patientsService.DeleteAsync(patientId);
+
+        return StatusCode(StatusCodes.Status204NoContent);
+    }
+
+    /// <summary>
+    /// Updates an existing patient.
+    /// </summary>
+    /// <param name="request">Data to update an existing patient.</param>
+    /// <returns>Empty result or expected errors.</returns>
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> Update([FromBody] UpdatePatientRequest request)
+    {
+        await _patientsService.UpdateAsync(request);
+
+        return StatusCode(StatusCodes.Status204NoContent);
+    }
+}
--- HCSYS.Api/Program.cs
+++ HCSYS.Api/Program.cs
@@ -1,28 +1,48 @@
 using HCSYS.Api.Middlewares;
 using HCSYS.Core;
 using HCSYS.Persistence;
+using System.Reflection;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 IConfiguration configuration = builder.Configuration;
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new()
+    {
+        Title = "Patients API",
+        Version = "v1",
+    });
+
+    Assembly entryAssembly = Assembly.GetExecutingAssembly();
+    string xmlFilename = $"{entryAssembly.GetName().Name}.xml";
+    string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFilename);
+
+    options.IncludeXmlComments(xmlPath);
+});
 
 builder.Services
     .AddPersistence(configuration.GetConnectionString("Default"))
     .AddCore();
 
+builder.Services.AddControllers();
+
 WebApplication app = builder.Build();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Channels API"));
 
 if (!app.Environment.IsDevelopment())
 {
     app.Services.MigrateDatabase();
 }
+
+app.MapControllers();
 
 app.Run();
